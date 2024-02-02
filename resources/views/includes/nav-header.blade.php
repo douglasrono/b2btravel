8c79f88ca3d16635b29133436ab4253420dbce86
douglasrono <douglasronno@gmail.com>
2024-02-02 17:19:25 +0300

    added project documentatiom and setup

--- resources/views/includes/nav-header.blade.php
+++ resources/views/includes/nav-header.blade.php
@@ -3,9 +3,9 @@
         <a href="/">
             <b class="logo-abbr"><img src="images/logo.png" alt>
             </b>
-            <span class="logo-compact"><img src="./images/logo-compact.png" alt></span>
+            <span class="logo-compact"><img src="./images/l-light.png" alt></span>
             <span class="brand-title">
-                <img src="images/logo-text.png" alt>
+                <img src="images/l-light.png" alt>
             </span>
         </a>
     </div>
